9655dd50c54e1077a8b48f3edd0b4170b297e904
Kevin Lam <kevin.quoct.lam@gmail.com>
2021-09-26 16:30:16 -0700

    test!7

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import { StaticImage } from 'gatsby-plugin-image'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial. Always most preferred</p>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="This is a cat picture on the internet"
         src="../images/image1.jpg"
