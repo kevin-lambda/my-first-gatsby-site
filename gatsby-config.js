18d56ad695135ca2375ad1c8e767e05e7cddeee7
Kevin Lam <kevin.quoct.lam@gmail.com>
2021-09-29 17:52:29 -0700

    test!9

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,17 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "My first gatsby site, a NEW title",
+    title: "My Super Cool Blog",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
-        path: `${__dirname}/blog`
-      }
-    }
+        path: `${__dirname}/blog/`,
+      },
+    },
+    "gatsby-plugin-mdx",
   ],
 };
