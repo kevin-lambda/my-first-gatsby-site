37d35c6bc4481da174a282158ab2b668b7aa868d
Kevin Lam <kevin.quoct.lam@gmail.com>
2021-10-01 00:08:10 -0700

    test!10

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,6 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-import Layout from '../components/layout'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
 
 const BlogPage = ({ data }) => {
     return (
@@ -10,9 +9,12 @@ const BlogPage = ({ data }) => {
         {
         data.allMdx.nodes.map((node) => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
-            <p>Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>{node.body}</MDXRenderer>
+            <h2> 
+              <Link to={`/blog/${node.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+            <p>Posted: {node.frontmatter.date}</p>        
           </article>
         ))
         }
@@ -31,7 +33,7 @@ query {
           title
         }
         id
-        body
+        slug
       }
     }
   }
